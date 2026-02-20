fac592f2b37c6617602c479450ce01e6d562d8cb
Shreenand Bhattad <145998920+Shreenandbhattad@users.noreply.github.com>
2026-02-21 02:35:36 +0530

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
 ﻿// Set this to your deployed backend URL when hosting frontend on GitHub Pages.
 // Example:
 // window.FLUX_API_BASE = "https://your-backend-service.onrender.com";
-window.FLUX_API_BASE = "";
+window.FLUX_API_BASE = "https://flux1.onrender.com";
